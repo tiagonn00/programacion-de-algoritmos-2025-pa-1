543c292d79ec808b74745e05721188d78e49bded
tiagonn <tiagomatinaguirre@gmail.com>
2025-09-03 21:15:46 +0000

    update tp 1

---
+++ tp1.tp2.java
@@ -0,0 +1,20 @@
+package tp1.tp2;
+
+public class Persona {
+Strig nombre;
+strin apellido;
+int edad;
+int dni;
+
+public persona(Strig nombre, Strig apellido, int edad, int dni)
+{
+    this.nombre=nombre;
+    this.apellido=apellido;
+    this.edad=edad;
+    this.dni=dni;
+}
+
+
+    
+
+ }
