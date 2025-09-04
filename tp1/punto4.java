b804d41e20cf88fb88b0ad6906eb93d7bd20d308
tiagonn <tiagomatinaguirre@gmail.com>
2025-09-04 20:41:16 +0000

    hago entrega de los ejercicios del Tp1

---
+++ tp1/punto4.java
@@ -0,0 +1,26 @@
+package tp1;
+
+import java.util.scanner;
+
+public class punto4; 
+
+
+        public static void main (String[] args) {
+        Scanner sc = new Scanner (System.in);
+
+        System.out.println("ingrese el primer numero");
+        int NRO1 = sc.nextItn();
+         
+        System.out.println("ingrese el segundo numero");
+        int NRO2 = sc.nextInt();
+
+        System.out.println("ingrese el tercer numero");
+        int NRO3 = sc.nextInt();
+         
+        if(NRO1 > NRO2 && NRO1 > NRO3) {
+            System.out.println("el mayor numero es:"+ NRO1);
+        } else if ( NRO2 > NRO1 )
+
+
+    }
+
